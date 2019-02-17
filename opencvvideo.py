8616a46cc64ec19c0ea5155e03a4cecf6fa8c361
raghavendra.udyavar <raghavs.mail@gmail.com>
2019-02-17 20:37:09 +0530

    removed unused variable

--- opencvvideo.py
+++ opencvvideo.py
@@ -5,7 +5,7 @@ video = cv2.VideoCapture(0)
 first_frame = None
 
 while True:
-    check, frame = video.read()
+    frame = video.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
@@ -30,7 +30,6 @@ while True:
     cv2.imshow("thresh", threshold_delta)
 
     key = cv2.waitKey(1)
-
     if key == ord('q') or cv2.getWindowProperty("capturing", 1) == -1:
         break
 
